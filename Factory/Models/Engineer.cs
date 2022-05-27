5c281c49253938bc650397df9badf841a6ebac3a
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-27 08:33:43 -0700

    initial commit with Models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<MachineEngineer>();
+        }
+
+        public int EngineerId { get; set; }
+        public string Name { get; set; }
+        
+        public int MachineId { get; set; }
+
+        public virtual ICollection<MachineEngineer> JoinEntities { get;}
+    }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+    {
+        public Machine()
+        {
+            //instantiates a new hashset upon creation
+            this.JoinEntities = new HashSet<MachineEngineer>();
+        }
+
+        public int MachineId { get; set; }
+        public string Name { get; set; }
+
+        public int EngineerId { get; set; }
+        public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
+    }
+}
