6485d89a56235733555815691c875c77d786df0e
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-05-27 11:58:51 -0700

    use viewbag to show total list of both models on splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
 
+     private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
+
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        //get list of both models in db, viewbag use in html file.
+        var engineerList = _db.Engineers.ToList();
+        var machineList = _db.Machines.ToList();
+        ViewBag.EngineerList = engineerList;
+        ViewBag.MachineList = machineList;
+      return View();
       }
 
     }
